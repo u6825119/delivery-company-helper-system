89346cecd6f3bdc0636fa33cee27390e15f37580
Jessica Lee <48383665+u6825119@users.noreply.github.com>
2023-08-22 19:53:21 +0800

    Delete Dijkstra.java

--- Dijkstra.java
+++
@@ -1,8 +0,0 @@
-public class Dijkstra {
-
-    /**Return the shortest path to the desired address ID**/
-    public int[] dijkstra(int[][] map, int addressID){
-     return new int[] {2,1,8};
-    }
-
-}
