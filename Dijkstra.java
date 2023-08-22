d0ec4d84149790faba412c1084f674bd8a01227c
Jessica Lee <48383665+u6825119@users.noreply.github.com>
2023-08-22 19:48:22 +0800

    Add files via upload

---
+++ Dijkstra.java
@@ -0,0 +1,8 @@
+public class Dijkstra {
+
+    /**Return the shortest path to the desired address ID**/
+    public int[] dijkstra(int[][] map, int addressID){
+     return new int[] {2,1,8};
+    }
+
+}
---
+++ RBTree.java
@@ -0,0 +1,9 @@
+public class RBTree {
+
+
+
+/**Find the order information by order ID**/
+public Order findByID(int ID){
+    return new Order(5, 20201013, "John", 8);
+}
+}
